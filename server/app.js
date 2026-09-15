9398a196f99a54d8546d264282150362fab56ba9
Prince Yadav <prince1bot001@gmail.com>
2026-09-15 15:18:21 +0530

    folder structure changed: added services folder

--- server/app.js
+++ server/app.js
@@ -5,6 +5,7 @@ dotenv.config();
 const express = require('express');
 const cors = require('cors');
 const connectDB = require('./database/db');
+const cookieParser = require('cookie-parser');
 
 connectDB();
 
@@ -12,6 +13,7 @@ const app = express();
 
 app.use(cors());
 app.use(express.json());
+app.use(cookieParser());
 
 app.use('/api/auth', authRoutes)
 
---
+++ server/services/authService.js
@@ -0,0 +1,76 @@
+const bcrypt = require('bcrypt');
+const validator = require('validator');
+const jwt = require('jsonwebtoken');
+
+const User = require('../models/User');
+
+const registerUser = async (name, emailId, password) => {
+
+    // 1. Validate required fields
+    if (
+        !name ||
+        !name.firstName ||
+        !name.lastName ||
+        !emailId ||
+        !password
+    ) {
+        throw new Error('All fields are required');
+    }
+
+    // 2. Validate email
+    if (!validator.isEmail(emailId)) {
+        throw new Error('Please enter a valid email');
+    }
+
+    // 3. Validate password
+    if (
+        !validator.isStrongPassword(password, {
+            minLength: 8,
+            minLowercase: 1,
+            minUppercase: 1,
+            minNumbers: 1,
+            minSymbols: 1
+        })
+    ) {
+        throw new Error(
+            'Password must contain at least 8 characters, one uppercase letter, one lowercase letter, one number, and one special character'
+        );
+    }
+
+    // 4. Check if user already exists
+    const existingUser = await User.findOne({ emailId });
+
+    if (existingUser) {
+        throw new Error('User already exists');
+    }
+
+    // 5. Hash password
+    const hashedPassword = await bcrypt.hash(password, 10);
+
+    // 6. Create user
+    const user = await User.create({
+        name,
+        emailId,
+        password: hashedPassword
+    });
+
+    // 7. Generate JWT token
+    const token = jwt.sign(
+        { userId: user._id },
+        process.env.JWT_SECRET,
+        { expiresIn: '7d' }
+    );
+
+    return {
+        token,
+        user: {
+            id: user._id,
+            name: user.name,
+            email: user.emailId
+        }
+    };
+};
+
+module.exports = {
+    registerUser
+};
